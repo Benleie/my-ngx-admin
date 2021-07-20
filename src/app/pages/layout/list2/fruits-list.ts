c3629b21444b5bd958a47319e30da03276471ab8
ben lee <awecg@qq.com>
2021-07-20 10:30:46 +0800

    <feat> list2 mocked list in layout

---
+++ src/app/pages/layout/list2/fruits-list.ts
@@ -0,0 +1,14 @@
+export const fruits: string[] = [
+    '柠檬',
+    'Raspberries',
+    '草莓',
+    '黑莓',
+    'Kiwis',
+    '葡萄',
+    'Avocado',
+    'Watermelon',
+    'Cantaloupe',
+    'Oranges',
+    'Peaches',
+];
+  
---
+++ src/app/pages/layout/list2/list2.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { fruits } from './fruits-list';
+
+@Component({
+  selector: 'ngx-list2',
+  templateUrl: './list2.component.html',
+  styleUrls: ['./list2.component.scss']
+})
+export class List2Component{
+  fruits = fruits;
+
+  users: { name: string, title: string }[] = [
+    { name: 'Carla Espinosa', title: 'Nurse' },
+    { name: 'Bob Kelso', title: 'Doctor of Medicine' },
+    { name: 'Janitor', title: 'Janitor' },
+    { name: 'Perry Cox', title: 'Doctor of Medicine' },
+    { name: 'Ben Sullivan', title: 'Carpenter and photographer' },
+  ];
+
+}
