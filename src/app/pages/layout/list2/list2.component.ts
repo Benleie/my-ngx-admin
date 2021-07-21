6e6610618d51c6a2a8a0e9955f28a73d65147fcf
Ben Lee <awecg@qq.com>
2021-07-21 11:05:56 +0800

    <feat> list2引入生命周期

--- src/app/pages/layout/list2/list2.component.ts
+++ src/app/pages/layout/list2/list2.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { fruits } from './fruits-list';
+import { NewsService } from '../news.service';
 
 @Component({
   selector: 'ngx-list2',
   templateUrl: './list2.component.html',
   styleUrls: ['./list2.component.scss'],
 })
-export class List2Component {
+export class List2Component implements OnInit {
   fruits = fruits;
 
   users: { name: string, title: string }[] = [
@@ -16,5 +17,14 @@ export class List2Component {
     { name: 'Perry Cox', title: 'Doctor of Medicine' },
     { name: 'Ben Sullivan', title: 'Carpenter and photographer' },
   ];
+
+  constructor(private newsService: NewsService) {}
+  ngOnInit() {
+    console.log('ngOnInit')
+    this.newsService.load(0, 3)
+      .subscribe(nextNews => {
+        console.log(nextNews)
+      });
+  }
 
 }
